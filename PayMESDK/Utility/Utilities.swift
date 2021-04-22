6d38520ecc1b51937ac0785e34c1d9bdf1e38ed4
khoatm <khoatm@payme.vn>
2021-04-22 16:11:53 +0700

    refactor structure source code

--- PayMESDK/Utility/Utilities.swift
+++ PayMESDK/Utility/Utilities.swift
@@ -1,5 +1,5 @@
 //
-//  utility.swift
+//  Utilities.swift
 //  PayMESDK
 //
 //  Created by HuyOpen on 12/31/20.
@@ -11,6 +11,7 @@ internal func handleColor(input: [String]) -> String {
     let newString = input.joined(separator: "\",\"")
     return newString
 }
+
 internal func checkIntNil(input: Int?) -> String {
     if input != nil {
         return String(input!)
@@ -33,12 +34,13 @@ internal func formatMoney(input: Int) -> String {
     return "\(temp!)"
 }
 
-internal func  urlFeENV(env: PayME.Env) -> String {
+internal func urlFeENV(env: PayME.Env) -> String {
     if (env == PayME.Env.SANDBOX) {
         return "https://sbx-wam.payme.vn/v1/"
     }
     return "https://wam.payme.vn/v1/"
 }
+
 internal func urlGraphQL(env: PayME.Env) -> String {
     if (env == PayME.Env.DEV) {
         return "https://dev-fe.payme.net.vn"
@@ -48,7 +50,7 @@ internal func urlGraphQL(env: PayME.Env) -> String {
     return "https://fe.payme.vn"
 }
 
-internal func  urlWebview(env: PayME.Env) -> String {
+internal func urlWebview(env: PayME.Env) -> String {
     if (env == PayME.Env.DEV) {
         return "https://sbx-sdk2.payme.com.vn/active/"
     } else if (env == PayME.Env.SANDBOX) {
@@ -63,17 +65,18 @@ internal func urlUpload(env: PayME.Env) -> String {
     }
     return "https://static.payme.vn/"
 }
+
 internal func trimKeyRSA(key: String) -> String {
     key.replacingOccurrences(of: "-----BEGIN PUBLIC KEY-----", with: "").replacingOccurrences(of: "-----END PUBLIC KEY-----", with: "").replacingOccurrences(of: "-----BEGIN RSA PRIVATE KEY-----", with: "").replacingOccurrences(of: "-----END RSA PRIVATE KEY-----", with: "").replacingOccurrences(of: "-----BEGIN PRIVATE KEY-----", with: "").replacingOccurrences(of: "-----END PRIVATE KEY-----", with: "")
 }
-internal func toastMess(title: String, message: String){
+
+internal func toastMess(title: String, message: String) {
     let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
     alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     PayME.currentVC?.present(alert, animated: true, completion: nil)
 }
 
-internal func toDateString(date: Date ) -> String
-{
+internal func toDateString(date: Date) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "HH:mm dd/MM/yyyy"
     print(dateFormatter.string(from: date))
@@ -106,10 +109,10 @@ internal func getMethodText(method: String) -> String {
     return method
 }
 
-internal func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+internal func convertStringToDictionary(text: String) -> [String: AnyObject]? {
     if let data = text.data(using: .utf8) {
         do {
-            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:AnyObject]
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject]
             return json
         } catch {
             print("Something went wrong")
@@ -117,21 +120,22 @@ internal func convertStringToDictionary(text: String) -> [String:AnyObject]? {
     }
     return nil
 }
-internal func checkCondition(onError: @escaping ([String:AnyObject]) -> ()) -> Bool {
+
+internal func checkCondition(onError: @escaping ([String: AnyObject]) -> ()) -> Bool {
     if (PayME.loggedIn == false || PayME.dataInit == nil) {
-        onError(["code": PayME.ResponseCode.ACCOUNT_NOT_LOGIN as AnyObject, "message" : "Vui lòng đăng nhập để tiếp tục" as AnyObject])
+        onError(["code": PayME.ResponseCode.ACCOUNT_NOT_LOGIN as AnyObject, "message": "Vui lòng đăng nhập để tiếp tục" as AnyObject])
         return false
     }
-    if !(Reachability.isConnectedToNetwork()){
-        onError(["code" : PayME.ResponseCode.NETWORK as AnyObject, "message" : "Vui lòng kiểm tra lại đường truyền mạng" as AnyObject])
+    if !(Reachability.isConnectedToNetwork()) {
+        onError(["code": PayME.ResponseCode.NETWORK as AnyObject, "message": "Vui lòng kiểm tra lại đường truyền mạng" as AnyObject])
         return false
     }
     if (PayME.accessToken == "") {
-        onError(["code" : PayME.ResponseCode.ACCOUNT_NOT_ACTIVATED as AnyObject, "message" : "Tài khoản chưa kích hoạt" as AnyObject])
+        onError(["code": PayME.ResponseCode.ACCOUNT_NOT_ACTIVATED as AnyObject, "message": "Tài khoản chưa kích hoạt" as AnyObject])
         return false
     }
     if (PayME.kycState != "APPROVED") {
-        onError(["code" : PayME.ResponseCode.ACCOUNT_NOT_KYC as AnyObject, "message" : "Tài khoản chưa định danh" as AnyObject])
+        onError(["code": PayME.ResponseCode.ACCOUNT_NOT_KYC as AnyObject, "message": "Tài khoản chưa định danh" as AnyObject])
         return false
     }
     return true
