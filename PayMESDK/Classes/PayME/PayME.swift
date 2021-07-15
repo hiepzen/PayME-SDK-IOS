415a783a0c844df7a40a63bfa777512590c7f451
khoatm <khoatm@mecorp.vn>
2021-07-15 19:04:18 +0700

    remove hàm get payment method

--- PayMESDK/Classes/PayME/PayME.swift
+++ PayMESDK/Classes/PayME/PayME.swift
@@ -162,19 +162,19 @@ public class PayME {
     }
 
     public func scanQR(
-            currentVC: UIViewController,
+            currentVC: UIViewController, payCode: String,
             onSuccess: @escaping (Dictionary<String, AnyObject>) -> (),
             onError: @escaping (Dictionary<String, AnyObject>) -> ()
     ) {
-        payMEFunction.openQRCode(currentVC: currentVC, onSuccess: onSuccess, onError: onError, isStartDirectFromUser: true)
+        payMEFunction.openQRCode(currentVC: currentVC, payCode: payCode, onSuccess: onSuccess, onError: onError, isStartDirectFromUser: true)
     }
 
     public func payQRCode(
-            currentVC: UIViewController, qr: String, isShowResultUI: Bool = true,
+            currentVC: UIViewController, qr: String, payCode: String, isShowResultUI: Bool = true,
             onSuccess: @escaping (Dictionary<String, AnyObject>) -> Void,
             onError: @escaping (Dictionary<String, AnyObject>) -> Void
     ) {
-        payMEFunction.payQRCode(currentVC: currentVC, qr: qr, isShowResultUI: isShowResultUI, onSuccess: onSuccess, onError: onError)
+        payMEFunction.payQRCode(currentVC: currentVC, qr: qr, payCode: payCode, isShowResultUI: isShowResultUI, onSuccess: onSuccess, onError: onError)
     }
 
     public func openKYC(
